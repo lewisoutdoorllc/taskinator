6dfd3f7c503f5dbaf934a4d9ce5e6b37f82fc32f
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-01-10 16:37:13 -0500

    updated js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,13 +5,21 @@ var taskFormHandler = function(event) {
         event.preventDefault();
 var taskNameInput = document.querySelector("input[name='task-name']").value;
 var taskTypeInput = document.querySelector("select[name='task-type']").value;
- // package up data as an object
+        // package up data as an object
  var taskDataObj = {
     name: taskNameInput,
-    type: taskTypeInput
-  };
+    type: taskTypeInput,
 
-  // send it as an argument to createTaskEl
+};
+        // check if input values are empty strings
+if (!taskNameInput || !taskTypeInput) {
+    alert("You need to fill out the task form!");
+    return false;
+}
+
+formEl.reset(); 
+
+        // send it as an argument to createTaskEl
   createTaskEl(taskDataObj);
 };
 
@@ -24,7 +32,7 @@ var taskInfoEl = document.createElement("div");
         taskInfoEl.className = "task-info";
         taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
         listItemEl.appendChild(taskInfoEl);
-// add entire list item to list
+        // add entire list item to list
         tasksToDoEl.appendChild(listItemEl);
 };
 
